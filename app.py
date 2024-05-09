771afeacdbf1e014677ea28bfc7b4da80c3e6a97
prinzeval <valentineallpowers@gmail.com>
2024-05-09 23:48:47 +0300

    c

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def disconnect():
     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    # Run the Flask app on port 80 for HTTP
+    socketio.run(app, host='0.0.0.0', port=80)
